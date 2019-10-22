66136299280c73b5641614865beddb82bbc24193
jackharrison111 <jackharrison111@gmail.com>
2019-10-22 16:11:38 +0100

    able to access map values now

--- converter.cxx
+++ converter.cxx
@@ -1,15 +1,21 @@
 #include "jsoncpp/src/lib_json/json_reader.cpp"
 #include "jsoncpp/src/lib_json/json_writer.cpp"
 #include "jsoncpp/src/lib_json/json_value.cpp"
+#include<map>
+#include<string>
+
+Json::StreamWriterBuilder wbuilder;
+map<string, map<string, double>> infos;
+
 int converter(){
 string jsonString;
-std::ifstream test_file("test.json");
+std::ifstream test_file("infofile.json");
 getline(test_file, jsonString); 
 
 
 Json::Value help;
-Json::CharReaderBuilder builder;
-Json::CharReader * reader = builder.newCharReader();
+Json::CharReaderBuilder rbuilder;
+Json::CharReader * reader = rbuilder.newCharReader();
 string errors;
 bool parseSuccess = reader->parse(jsonString.c_str(),jsonString.c_str() + jsonString.size(), &help, &errors);
 
@@ -18,8 +24,35 @@ delete reader;
 if (!parseSuccess){
 	std::cout<<"Error in parsing json file" << std::endl;
 }
-std::cout<<help["Graviton1500"]["xsec"];
 
+for(Json::Value::const_iterator outer = help.begin(); outer != help.end(); outer++){
+	//std::cout<<outer.key()<<std::endl;
+	map<string, double> data;
+	for(Json::Value::const_iterator inner = (*outer).begin() ; inner != (*outer).end(); inner++){
+		//std::cout << "  " << inner.key() << " : " << *inner << std::endl;
+		string parName = Json::writeString(wbuilder,inner.key());
+		double parValue = stod(Json::writeString(wbuilder,*inner));
+
+		data.insert(pair<string, double>(parName, parValue));
+	}
+
+		
+
+
+
+
+	string outerName = Json::writeString(wbuilder, outer.key());
+	infos.insert(pair<string, map<string, double>>(outerName, data));
+}
+
+for(map<string, map<string, double>>::iterator it = infos.begin(); it!= infos.end(); it++){
+	std::cout << it->first << std::endl;
+	for(map<string, double>::iterator at = (it->second).begin(); at!=(it->second).end(); at++){
+		std::cout<<"   "<<at->first<<" : "<<at->second<<std::endl;
+	}
+}
+
+//std::cout<<(infos["Graviton1500"])["sumw"]<<std::endl;
 
 
 return 0;
