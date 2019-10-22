4ffb89d7a13d8e8bb5e464ea6c999c872801c77b
aburke1605 <aodhan.burke@student.manchester.ac.uk>
2019-10-22 12:31:46 +0100

    changes

--- converter.cxx
+++ converter.cxx
@@ -1,13 +1,7 @@
-#include "jsoncpp/src/lib_json/json_reader.cpp"
-//#include <fstream>
-#include "jsoncpp/src/lib_json/json_writer.cpp"
-#include "jsoncpp/src/lib_json/json_value.cpp"
-//#include "jsoncpp/include/json/reader.h"
-#include <string>
+#include "json/jsoncpp/src/lib_json/json_reader.cpp"
+#include "json/jsoncpp/src/lib_json/json_writer.cpp"
+#include "json/jsoncpp/src/lib_json/json_value.cpp"
 int converter(){
-
-
-
 string jsonString;
 std::ifstream test_file("test.json");
 getline(test_file, jsonString); 
@@ -22,7 +16,7 @@ bool parseSuccess = reader->parse(jsonString.c_str(),jsonString.c_str() + jsonSt
 delete reader;
 
 if (!parseSuccess){
-	std::cout<<"FuCk SAkEE ALi" << std::endl;
+	std::cout<<"Error in parsing json file" << std::endl;
 }
 std::cout<<help["Graviton1500"]["xsec"];
 
