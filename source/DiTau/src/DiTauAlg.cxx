8988a0766c6ca657fc84e826cb179e2525479ac8
aburke1605 <aodhan.burke@student.manchester.ac.uk>
2020-03-10 18:02:30 +0100

    IMETMaker.h

--- source/DiTau/src/DiTauAlg.cxx
+++ source/DiTau/src/DiTauAlg.cxx
@@ -6,6 +6,7 @@
 #include "DiTauAlg.h"
 #include "xAODJet/JetContainer.h"
 #include "xAODMissingET/MissingETContainer.h"
+#include "METInterface/IMETMaker.h"
 
 // core EDM includes
 #include "AthContainers/AuxElement.h"
